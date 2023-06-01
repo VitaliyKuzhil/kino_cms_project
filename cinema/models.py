695ce7d8cd7dd9e01843f81dacf047bc2fbc3c16
Vitaliy Kuzhil <vitaliy.kuzhil2018@gmail.com>
2023-06-01 15:13:52 +0300

    Add core and cinema models

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,104 @@
 from django.db import models
 
-# Create your models here.
+
+class Seo(models.Model):  # Model SEO
+    id_seo = models.AutoField(primary_key=True)
+    url_seo = models.URLField(max_length=300, null=True, blank=True)
+    title_seo = models.CharField(max_length=300, null=False, blank=False)
+    keywords_seo = models.CharField(max_length=400, null=False, blank=False)
+    description_seo = models.TextField(null=True, blank=True)
+
+
+class Gallery(models.Model):  # Model Gallery
+    id_gallery = models.AutoField(primary_key=True)
+    name_gallery = models.CharField(max_length=300, null=False, blank=False, unique=True)
+
+
+class Photos(models.Model):  # Model Photo
+    id_photo = models.AutoField(primary_key=True)
+    gallery = models.ManyToManyField(Gallery, through='GalleryPhotos', null=True, blank=True)
+    photo = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False)
+
+
+class GalleryPhotos(models.Model):  # Connection ManyToMany between Gallery and Photos
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    photos = models.ForeignKey(Photos, on_delete=models.CASCADE)
+
+
+class Cinemas(models.Model):  # Model Cinemas
+    id_cinema = models.AutoField(primary_key=True)
+    name_cinema = models.CharField(max_length=300, null=False, blank=False)
+    description_cinema = models.TextField(null=True, blank=True)
+    logo_cinema = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+    main_foto_cinema = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+    amenities_cinema = models.CharField(max_length=500, null=True, blank=True)
+    gallery_cinema = models.OneToOneField(Gallery, on_delete=models.CASCADE, blank=True, null=True)
+    seo_cinema = models.OneToOneField(Seo, on_delete=models.CASCADE, blank=True, null=True)
+    data_create_cinema = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+
+    def __str__(self):
+        return f'Cinema: {self.name_cinema}'
+
+    class Meta:
+        verbose_name = 'Cinema'
+        verbose_name_plural = 'Cinemas'
+        ordering = ['name_cinema']
+
+
+class Halls(models.Model):  # Model Halls
+    id_hall = models.AutoField(primary_key=True)
+    cinema_hall = models.ForeignKey(Cinemas, on_delete=models.CASCADE, blank=False, null=False)
+    number_hall = models.IntegerField(default=1, blank=False, null=False)
+    description_hall = models.TextField(null=True, blank=True)
+    photo_shem_hall = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+    main_foto_hall = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+    count_seats_hall = models.IntegerField(default=1, blank=True, null=True)
+    gallery_hall = models.OneToOneField(Gallery, on_delete=models.CASCADE, blank=True, null=True)
+    seo_hall = models.OneToOneField(Seo, on_delete=models.CASCADE, blank=True, null=True)
+    data_create_hall = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+
+    def __str__(self):
+        return f'Hall: {self.number_hall}'
+
+    class Meta:
+        verbose_name = 'Hall'
+        verbose_name_plural = 'Halls'
+        ordering = ['number_hall']
+
+
+class Rows(models.Model):  # Model Rows
+    id_row = models.AutoField(primary_key=True)
+    row_hall = models.ForeignKey(Halls, on_delete=models.CASCADE, blank=False,
+                                 null=False)  # Connection ForeignKey between Halls and Rows
+
+
+class Seats(models.Model):  # Model Seats
+    id_seat = models.AutoField(primary_key=True)
+    seat_row = models.ManyToManyField(Rows, through='SeatsRows', null=False, blank=False)
+    status_seat = models.BooleanField(default=False, blank=True, null=True)
+
+
+class SeatsRows(models.Model):  # Connection ManyToMany between Rows and Seats
+    seat = models.ForeignKey(Seats, on_delete=models.CASCADE)
+    row = models.ForeignKey(Rows, on_delete=models.CASCADE)
+
+
+class Movies(models.Model):  # Model Movies
+    id_movie = models.AutoField(primary_key=True)
+    name_movie = models.CharField(max_length=300, null=False, blank=False)
+    description_movie = models.TextField(null=True, blank=True)
+    type_movie = models.CharField(max_length=70, null=False, blank=False)
+    main_foto_movie = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+    status_movie = models.CharField(max_length=50, null=True, blank=True)
+    url_movie = models.URLField(max_length=300, null=True, blank=True)
+    gallery_movie = models.OneToOneField(Gallery, on_delete=models.CASCADE, null=True, blank=True)
+    seo_movie = models.OneToOneField(Seo, on_delete=models.CASCADE, null=True, blank=True)
+    data_create_movie = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return f'Movie: {self.name_movie}'
+
+    class Meta:
+        verbose_name = 'Movie'
+        verbose_name_plural = 'Movies'
+        ordering = ['name_movie']
--- core/models.py
+++ core/models.py
@@ -1,3 +1,174 @@
 from django.db import models
+from cinema.models import Seo, Gallery
+from datetime import datetime
 
-# Create your models here.
+
+class Pages(models.Model):  # Model Pages
+    id_page = models.AutoField(primary_key=True)
+    name_page = models.CharField(max_length=50, null=False, blank=False)
+    gallery_page = models.OneToOneField(Gallery, on_delete=models.CASCADE, null=True, blank=True)
+    seo_page = models.OneToOneField(Seo, on_delete=models.CASCADE, null=True, blank=True)
+    status_page = models.BooleanField(default=False, null=False, blank=False)
+    data_create_page = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+
+    def __str__(self):
+        return f'Page: {self.name_page}'
+
+    class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+        ordering = ['data_create_page']
+
+
+class HomePage(Pages):  # Model HomePage
+    id_home_page = models.AutoField(primary_key=True)
+    photo1 = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+    photo2 = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name_page
+
+
+class NewsSharesPage(models.Model):  # Abstract Model NewsSharesPage
+    id_news_shares = models.AutoField(primary_key=True)
+    id_main_page = models.ForeignKey(Pages, on_delete=models.CASCADE, null=False, blank=False)
+    type_page = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    main_photo = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+    url_address = models.URLField(max_length=300, null=True, blank=True)
+    data_published = models.DateField(default=datetime.now)
+
+    class Meta:
+        abstract = True
+
+
+class NewsPage(NewsSharesPage):  # Model NewsPage
+    pass
+
+
+class SharesPage(NewsSharesPage):  # Model SharesPage
+    pass
+
+
+class CafeBarPage(Pages):  # Model CafeBarPage
+    title = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    main_photo = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+    menu_cafe_bar = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name_page
+
+
+class VipHallPage(Pages):  # Model VipHallPage
+    title = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    main_photo = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name_page
+
+
+class AdvertisePage(Pages):  # Model AdvertisePage
+    title = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    main_photo = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name_page
+
+
+class ChildrenRoomPage(Pages):  # Model ChildrenRoomPage
+    title = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    main_photo = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+
+
+class ContactPage(Pages):  # Model ContactPage
+    id_cinema_contact = models.AutoField(primary_key=True)
+    logo_cinema = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+    name_cinema = models.CharField(max_length=100, null=False, blank=False)
+    address_cinema = models.CharField(max_length=200, null=False, blank=False)
+    location_cinema = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.name_page
+
+
+class Banners(models.Model):  # Abstract Model Banners
+    id_banner = models.AutoField(primary_key=True)
+    name_banner = models.CharField(max_length=50, null=False, blank=False)
+    status_banner = models.CharField(max_length=50, null=False, blank=False) # choice
+
+    def __str__(self):
+        return f'Banner: {self.name_banner}'
+
+    class Meta:
+        abstract = True
+        verbose_name = 'Banner'
+        verbose_name_plural = 'Banners'
+
+
+class HomeBanner(Banners):  # Model HomeBanner
+    gallery_banner = models.OneToOneField(Gallery, on_delete=models.CASCADE, blank=True, null=True)
+    url_banner = models.URLField(max_length=300, null=True, blank=True)
+    text_banner = models.TextField(null=True, blank=True)
+    speed_banner = models.IntegerField(default=5, null=True, blank=True)
+
+
+class HomeNewsSharesBanner(Banners):  # Model HomeNewsSharesBanner
+    gallery_banner = models.OneToOneField(Gallery, on_delete=models.CASCADE, blank=True, null=True)
+    url_banner = models.URLField(max_length=300, null=True, blank=True)
+    speed_banner = models.IntegerField(default=5, null=True, blank=True)
+
+
+class BackgroundBanner(Banners):  # Model BackgroundBanner
+    gallery_banner = models.OneToOneField(Gallery, on_delete=models.CASCADE, blank=True, null=True)
+
+
+class ContextAdvertise(models.Model):  # Model ContextAdvertise
+    id_context = models.AutoField(primary_key=True)
+    photo_context = models.ImageField(upload_to='static/photos/%Y/%m/%d/', editable=False, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Context'
+        verbose_name_plural = 'Contexts'
+
+
+class MobileApplication(models.Model):  # Model MobileApplication
+    id_application = models.AutoField(primary_key=True)
+    name_application = models.CharField(max_length=50, null=False, blank=False)
+    description_application = models.TextField(null=True, blank=True)
+    gallery_application = models.OneToOneField(Gallery, on_delete=models.CASCADE, blank=True, null=True)
+    url_application = models.URLField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return self.name_application
+
+    class Meta:
+        verbose_name = 'Application'
+        verbose_name_plural = 'Applications'
+        ordering = ['name_application']
+
+
+class SocialMedia(models.Model):  # Model SocialMedia
+    id_social = models.AutoField(primary_key=True)
+    name_social = models.CharField(max_length=50, null=False, blank=False)
+    url_social = models.URLField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return self.name_social
+
+    class Meta:
+        verbose_name = 'Application'
+        verbose_name_plural = 'Applications'
+        ordering = ['name_social']
+
+
+class MailingTemplates(models.Model):  # Model MailingTemplates
+    id_mailing = models.AutoField(primary_key=True)
+    template_mailing = models.FileField(upload_to='static/mailing/%Y/%m/%d/', editable=False, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'MailingTemplate'
+        verbose_name_plural = 'MailingTemplates'
