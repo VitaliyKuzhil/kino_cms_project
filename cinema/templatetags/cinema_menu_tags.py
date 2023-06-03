3466885acc690e338d8f550cb1f99950b6f5c285
Vitaliy Kuzhil <vitaliy.kuzhil2018@gmail.com>
2023-06-03 10:15:46 +0300

    Modified base templates into the all aplications

--- cinema/templatetags/cinema_menu_tags.py
+++ cinema/templatetags/cinema_menu_tags.py
@@ -6,12 +6,17 @@ register = template.Library()
 
 @register.inclusion_tag('cinema/nav_bar.html', name='nav_bar')
 def cinema_menu():
-    nav_bar = [{'title': "Афіша"},
-               {'title': "Розклад"},
-               {'title': "Скоро"},
-               {'title': "Кінотеатри"},
-               {'title': "Акції"},
+    nav_bar = [{'title': "Головна", 'url': 'cinema:home_page'},
+               {'title': "Афіша", 'url': 'cinema:list_films'},
+               {'title': "Розклад", 'url': 'cinema:sessions'},
+               {'title': "Скоро", 'url': 'cinema:list_films'},
+               {'title': "Кінотеатри", 'url': 'cinema:list_cinemas'},
+               {'title': "Акції", 'url': 'core:shares'},
                {'title': "Про кінотеатр",
-                'list': {'Новини': "home", 'Реклама': "addcinema", 'Кафе': "home", 'Мобільний додаток': 'cdc',
-                         'Контакти': 'ece'}}]
+                'list': ({'title': 'Новини', 'url': 'cinema:list_films'},
+                         {'title': 'Реклама', 'url': 'cinema:list_films'},
+                         {'title': 'Кафе', 'url': 'cinema:list_films'},
+                         {'title': 'Мобільний додаток', 'url': 'cinema:list_films'},
+                         {'title': 'Контакти', 'url': 'cinema:list_films'})
+                }]
     return {'nav_bar': nav_bar}
--- user/templatetags/admin_menu_tags.py
+++ user/templatetags/admin_menu_tags.py
@@ -4,21 +4,18 @@ from user.models import *
 register = template.Library()
 
 
-@register.inclusion_tag('user/side_bar.html', name='side_bar')
-def cinema_menu():
-    side_bar = [{'title': "Статистика", 'url': 'home', 'icon': 'fas fa-chart-pie', 'list': {}},
-            {'title': "Банера", 'url': 'baners', 'icon': 'far fa-image', 'list': {}},
-            {'title': "Фільми", 'url': 'add_film', 'icon': 'fa-sharp fa-solid fa-film',
-             'list': {'Список фільмів': "list_films", 'Добавити фільм': "add_film"}},
-            {'title': "Кінотеатри", 'url': 'add_cinema', 'icon': 'fa-solid fa-user',
-             'list': {'Список кінотеатрів': "list_cinemas", 'Добавити кінотеатр': "add_cinema", 'Добавити зал': "add_hall"}},
-            {'title': "Новини", 'url': 'news', 'icon': 'fa-solid fa-newspaper', 'list': {'Добавити новину': "add_news"}},
-            {'title': "Акції", 'url': 'shares', 'icon': 'fa-regular fa-star', 'list': {'Добавити акцію': "add_share"}},
-            {'title': "Сторінки", 'url': 'pages', 'icon': 'fa-solid fa-table-list',
-             'list': {'Головна сторінка': "home", "Про кінотеатр": "home", "Кафе-Бар": "cafe_bar", "Vip-зал": "vip_hall",
-                      "Реклама": "adv", "Дитяча кімната": "children_room", "Контакти": "contacts"}},
-            {'title': "Користувачі", 'url': 'list_users', 'icon': 'fa-solid fa-user', 'list': {}},
-            {'title': "Розсилка", 'url': 'mailing', 'icon': 'fa-solid fa-envelopes-bulk', 'list': {}}
-            ]
-    return {'side_bar': side_bar}
+@register.inclusion_tag('user/side_bar.html')
+def render_menu_items():
+    side_bar = [
+        {'title': "Статистика", 'url': 'user:statistic', 'icon': 'fa-chart-line'},
+        {'title': "Банера", 'url': 'user:banners', 'icon': 'fa-image'},
+        {'title': "Фільми", 'url': 'cinema:list_films', 'icon': 'fa-film'},
+        {'title': "Кінотеатри", 'url': 'cinema:list_cinemas', 'icon': 'fa-store'},
+        {'title': "Новини", 'url': 'core:news', 'icon': 'fa-newspaper'},
+        {'title': "Акції", 'url': 'core:shares', 'icon': 'fa-star'},
+        {'title': "Сторінки", 'url': 'core:list_pages', 'icon': 'fa-layer-group'},
+        {'title': "Користувачі", 'url': 'user:list_users', 'icon': 'fa-user'},
+        {'title': "Розсилка", 'url': 'core:mailing', 'icon': 'fa-people-arrows'}
+    ]
+    return {'menu_items': side_bar}
 
