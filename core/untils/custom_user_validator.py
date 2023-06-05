4b42b0cf7d01d6608859ccfac46a2b115543a0d0
Vitaliy Kuzhil <vitaliy.kuzhil2018@gmail.com>
2023-06-05 23:40:11 +0300

    Minor corrections in user_validator and base_user_template

--- core/untils/custom_user_validator.py
+++ core/untils/custom_user_validator.py
@@ -3,9 +3,9 @@ from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
 
-class UserValidator:
+class UserValidator:  # Validator for CustomUser model
     @staticmethod
-    def birthday_validator(value):
+    def birthday_validator(value):  # Check birthday day
         current_date = timezone.now().date()
         if value >= current_date:
-            raise ValidationError(_('Invalid birthday format.'))
+            raise ValidationError(_('Invalid birthday day.'))
