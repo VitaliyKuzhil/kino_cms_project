611ef4b7c9bacba26576fd27d2b8e36324cc156d
Vitaliy Kuzhil <vitaliy.kuzhil2018@gmail.com>
2023-06-05 14:49:54 +0300

    Register CustomUser with CustomUserAdmin into admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'phone', 'city',
+                    'gender', 'birthday', 'language', 'number_card']
+    search_fields = ['first_name', 'last_name']
+    list_filter = ['gender', 'language', 'city']
+    # list_editable = []
+    fields = ['first_name', 'last_name', 'email', 'phone', 'city',
+              'gender', 'birthday', 'language', 'number_card']
+    # readonly_fields = ['create_time', 'update_time']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
