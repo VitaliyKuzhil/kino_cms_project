225b6ee4a3f2bd23d10ccc152052597230105a8d
Vitaliy Kuzhil <vitaliy.kuzhil2018@gmail.com>
2023-06-05 14:51:57 +0300

    Add User authorization model into settings

--- kino_cms/settings.py
+++ kino_cms/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-gtu$r9&g(kd^qgm-*@_p2rt*u#(6rjxo8299el7n-xr*5-u1^y
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kino_cms.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -88,6 +85,8 @@ DATABASES = {
     }
 }
 
+#  User authorization model
+AUTH_USER_MODEL = "user.CustomUser"  # Custom User Model
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -107,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -119,7 +117,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -128,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Add base dir for static files
 STATICFILES_DIRS = []
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')   # Add base dir for media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Add base dir for media files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
