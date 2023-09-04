62ad4b72594c159f9c045deb046045405c86279c
Vitaliy Kuzhil <vitaliy.kuzhil2018@gmail.com>
2023-09-04 11:46:32 +0300

    Add templatetags for cinema and user app

--- cinema/templatetags/cinema_menu_tags.py
+++ cinema/templatetags/cinema_menu_tags.py
@@ -1,22 +1,37 @@
 from django import template
-from cinema.models import *
+from core.models import *
 
 register = template.Library()
 
 
 @register.inclusion_tag('cinema/nav_bar.html', name='nav_bar')
 def cinema_menu():
-    nav_bar = [{'title': "Головна", 'url': 'cinema:home_page'},
-               {'title': "Афіша", 'url': 'cinema:list_films'},
-               {'title': "Розклад", 'url': 'cinema:sessions'},
-               {'title': "Скоро", 'url': 'cinema:list_films'},
-               {'title': "Кінотеатри", 'url': 'cinema:list_cinemas'},
-               {'title': "Акції", 'url': 'core:shares'},
-               {'title': "Про кінотеатр",
-                'list': ({'title': 'Новини', 'url': 'cinema:list_films'},
-                         {'title': 'Реклама', 'url': 'cinema:list_films'},
-                         {'title': 'Кафе', 'url': 'cinema:list_films'},
-                         {'title': 'Мобільний додаток', 'url': 'cinema:list_films'},
-                         {'title': 'Контакти', 'url': 'cinema:list_films'})
-                }]
+    home = HomePage.objects.filter(status_page=True).first()
+    about_cinema = AboutCinemaPage.objects.filter(status_page=True).first()
+    cafe_bar = CafeBarPage.objects.filter(status_page=True).first()
+    vip_hall = VipHallPage.objects.filter(status_page=True).first()
+    advertise = AdvertisePage.objects.filter(status_page=True).first()
+    children_room = ChildrenRoomPage.objects.filter(status_page=True).first()
+    contacts = Pages.objects.filter(status_page=True).get(name_page='Contacts page')
+
+    nav_bar = [{'title': home.name_page, 'url': 'cinema:home_page'} if home else None,
+               {'title': "Poster", 'url': 'cinema:list_films_all'},
+               {'title': "Sessions", 'url': 'cinema:sessions'},
+               {'title': "Soon", 'url': 'cinema:list_films_soon'},
+               {'title': "Cinemas", 'url': 'cinema:list_cinemas'},
+               {'title': "Shares", 'url': 'cinema:shares_user'},
+               {'title': about_cinema.name_page, 'url': 'cinema:about_cinema_user',
+                'list': [{'title': 'News', 'url': 'cinema:news_user'},
+                         {'title': cafe_bar.name_page, 'url': 'cinema:cafe_bar_user'} if cafe_bar else None,
+                         {'title': vip_hall.name_page, 'url': 'cinema:vip_hall_user'} if vip_hall else None,
+                         {'title': children_room.name_page, 'url': 'cinema:children_room_user'} if children_room else None,
+                         {'title': advertise.name_page, 'url': 'cinema:advertise_user'} if advertise else None,
+                         {'title': 'Mobie app', 'url': 'cinema:mobile_app_user'},
+                         {'title': contacts.name_page, 'url': 'cinema:contacts_user'} if contacts else None,
+                         *[
+                             {'title': page.name_page, 'url': 'cinema:custom_page'}
+                             for page in CustomPage.objects.filter(status_page=True)
+                         ]
+                         ]
+                } if about_cinema else None]
     return {'nav_bar': nav_bar}
--- user/templatetags/admin_menu_tags.py
+++ user/templatetags/admin_menu_tags.py
@@ -7,15 +7,15 @@ register = template.Library()
 @register.inclusion_tag('user/side_bar.html')
 def render_menu_items():
     side_bar = [
-        {'title': "Статистика", 'url': 'user:statistic', 'icon': 'fa-chart-line'},
-        {'title': "Банера", 'url': 'user:banners', 'icon': 'fa-image'},
-        {'title': "Фільми", 'url': 'cinema:list_films', 'icon': 'fa-film'},
-        {'title': "Кінотеатри", 'url': 'cinema:list_cinemas', 'icon': 'fa-store'},
-        {'title': "Новини", 'url': 'core:news', 'icon': 'fa-newspaper'},
-        {'title': "Акції", 'url': 'core:shares', 'icon': 'fa-star'},
-        {'title': "Сторінки", 'url': 'core:list_pages', 'icon': 'fa-layer-group'},
-        {'title': "Користувачі", 'url': 'user:list_users', 'icon': 'fa-user'},
-        {'title': "Розсилка", 'url': 'core:mailing', 'icon': 'fa-people-arrows'}
+        {'title': "Statistic", 'url': 'user:statistic_admin', 'icon': 'fa-chart-line'},
+        {'title': "Banners", 'url': 'user:banners_admin', 'icon': 'fa-image'},
+        {'title': "Movies", 'url': 'user:list_films_admin', 'icon': 'fa-film'},
+        {'title': "Cinemas", 'url': 'user:list_cinemas_admin', 'icon': 'fa-store'},
+        {'title': "News", 'url': 'user:news_admin', 'icon': 'fa-newspaper'},
+        {'title': "Shares", 'url': 'user:shares_admin', 'icon': 'fa-star'},
+        {'title': "Pages", 'url': 'user:list_pages_admin', 'icon': 'fa-layer-group'},
+        {'title': "Users", 'url': 'user:list_users_admin', 'icon': 'fa-user'},
+        {'title': "Mailing", 'url': 'user:mailing_admin', 'icon': 'fa-people-arrows'}
     ]
     return {'menu_items': side_bar}
 
